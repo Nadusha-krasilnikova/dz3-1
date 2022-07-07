ea1ac71c9ccafec9a52851003975aa5024dbfc17
Nadusha <nadusha230275@gmail.com>
2022-07-07 21:14:21 +0500

    работает

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// найти кубы чисел от 1 до N
+Console.WriteLine("введите число ");
+int a= int.Parse(Console.ReadLine());
+ for (int i=1; i<=a; i++)
+ {
+    Console.WriteLine($"{i}x{i}x{i}={i*i*i}");
+ }
